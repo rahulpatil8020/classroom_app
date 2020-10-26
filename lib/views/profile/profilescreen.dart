90b8a50c075c9ebbfe376a99e292a57300a5dc68
rahulpatil8020 <rahuldpatil20@gmail.com>
2020-10-26 12:58:40 +0530

    ..

--- lib/views/profile/profilescreen.dart
+++ lib/views/profile/profilescreen.dart
@@ -5,27 +5,26 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: [
-        SizedBox(
-          height: 20,
-        ),
-        Align(
-          alignment: Alignment.topCenter,
-          child: Container(
-            height: 140,
-            width: 140,
-            decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: kPrimaryColor,
-                border: Border.all(
-                    color: kSecondaryColor.withOpacity(0.2),
-                    width: 0.3,
-                    style: BorderStyle.solid)),
+        body: Align(
+      alignment: Alignment.topCenter,
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 15.0),
+            child: Container(
+              height: 140,
+              width: 140,
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: kPrimaryColor,
+                  border: Border.all(
+                      color: kSecondaryColor.withOpacity(0.2),
+                      width: 0.3,
+                      style: BorderStyle.solid)),
+            ),
           ),
-        )
-      ],
+        ],
+      ),
     ));
   }
 }
