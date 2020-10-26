10cf16bff21d11b59f2b03c8888a9c4d5396a424
Tanmay Gangurde <48478512+imtanbro@users.noreply.github.com>
2020-10-26 23:12:43 +0530

    .

--- lib/views/signup/signup.dart
+++ lib/views/signup/signup.dart
@@ -115,245 +115,247 @@ class _SignUpState extends State<SignUp> {
                 ),
               ),
             )
-          : Form(
-                    key: _formKey,
-                    child: Container(
-          margin: EdgeInsets.symmetric(horizontal: 24, vertical: 20),
-          child: Column(
-            children: [
-              TextFormField(
-            validator: (val) {
-              return val.isEmpty ? "Enter First Name" : null;
-            },
-            decoration: InputDecoration(
-              hintText: "First Name",
-            ),
-            onChanged: (val) {
-              fname = val;
-            },
+          : SingleChildScrollView(
+                      child: Form(
+                      key: _formKey,
+                      child: Container(
+            margin: EdgeInsets.symmetric(horizontal: 24, vertical: 20),
+            child: Column(
+              children: [
+                TextFormField(
+              validator: (val) {
+                return val.isEmpty ? "Enter First Name" : null;
+              },
+              decoration: InputDecoration(
+                hintText: "First Name",
               ),
-              SizedBox(
-            height: size.height * 0.01,
-              ),
-              TextFormField(
-            validator: (val) {
-              return val.isEmpty ? "Enter Middle Name" : null;
-            },
-            decoration: InputDecoration(
-              hintText: "Middle Name",
-            ),
-            onChanged: (val) {
-              mname = val;
-            },
-              ),
-              SizedBox(
-            height: size.height * 0.01,
-              ),
-              TextFormField(
-            validator: (val) {
-              return val.isEmpty ? "Enter Last Name" : null;
-            },
-            decoration: InputDecoration(
-              hintText: "Last Name",
-            ),
-            onChanged: (val) {
-              lname = val;
-            },
-              ),
-              SizedBox(
-            height: size.height * 0.01,
-              ),
-              Row(children: [
-            SizedBox(
-              width: 152,
-              child: TextFormField(
-                validator: (val) {
-                  return val.length <= 5 && val.isNotEmpty
-            ? null
-            : "Roll number should be of 2 digits";
-                },
-                decoration: InputDecoration(
-                  hintText: "Roll No.(5 digit)",
+              onChanged: (val) {
+                fname = val;
+              },
                 ),
-                keyboardType: TextInputType.number,
-                onChanged: (val) {
-                  rollno = val;
-                },
-                cursorWidth: 10,
-              ),
-            ),
-            SizedBox(
-              width: 20,
-            ),
-            SizedBox(
-              width: 152,
-              child: TextFormField(
-                validator: (val) {
-                  return val == "A" || val == 'B'
-            ? null
-            : "Enter either A or B";
-                },
-                decoration: InputDecoration(
-                  hintText: "Div.",
+                SizedBox(
+              height: size.height * 0.01,
                 ),
-                
-                onChanged: (val) {
-                  div = val;
-                },
-                cursorWidth: 10,
+                TextFormField(
+              validator: (val) {
+                return val.isEmpty ? "Enter Middle Name" : null;
+              },
+              decoration: InputDecoration(
+                hintText: "Middle Name",
               ),
-            ),
-              ]),
+              onChanged: (val) {
+                mname = val;
+              },
+                ),
+                SizedBox(
+              height: size.height * 0.01,
+                ),
+                TextFormField(
+              validator: (val) {
+                return val.isEmpty ? "Enter Last Name" : null;
+              },
+              decoration: InputDecoration(
+                hintText: "Last Name",
+              ),
+              onChanged: (val) {
+                lname = val;
+              },
+                ),
+                SizedBox(
+              height: size.height * 0.01,
+                ),
+                Row(children: [
               SizedBox(
-            height: size.height * 0.01,
-              ),
-              ListTile(
-            title: Text(
-                "Date of Birth : ${pickeddate.day}/${pickeddate.month}/${pickeddate.year}"),
-            trailing: Icon(Icons.keyboard_arrow_down),
-            onTap: _pickDate,
-              ),
-              Row(
-            children: [
-              DropdownButton<String>(
-                items: [
-                  DropdownMenuItem(
-            value: "Computer Engineering",
-            child: Text("Computer Engineering")),
-                  DropdownMenuItem(
-            value: "E&TC", child: Text("E&TC")),
-                  DropdownMenuItem(
-            value: "Mechanical Engineering",
-            child: Text("Mechanical Engineering")),
-                  DropdownMenuItem(
-            value: "Civil Engineering",
-            child: Text("Civil Engineering")),
-                  DropdownMenuItem(
-            value: "Chemical Engineering",
-            child: Text("Chemical Engineering")),
-                ],
-                onChanged: (val) {
-                  setState(() {});
-                  branch = val;
-                  print(branch);
-                },
-                hint: Text("Branch"),
-                value: branch,
-              ),
-              SizedBox(
-                width: 40,
-              ),
-              DropdownButton<String>(
-                items: [
-                  DropdownMenuItem(
-            value: "Sem 1", child: Text("Sem 1")),
-                  DropdownMenuItem(
-            value: "Sem 2", child: Text("Sem 2")),
-                  DropdownMenuItem(
-            value: "Sem 3", child: Text("Sem 3")),
-                  DropdownMenuItem(
-            value: "Sem 4", child: Text("Sem 4")),
-                  DropdownMenuItem(
-            value: "Sem 5", child: Text("Sem 5")),
-                  DropdownMenuItem(
-            value: "Sem 6", child: Text("Sem 6")),
-                  DropdownMenuItem(
-            value: "Sem 7", child: Text("Sem 7")),
-                  DropdownMenuItem(
-            value: "Sem 8", child: Text("Sem 8")),
-                ],
-                onChanged: (val) {
-                  setState(() {
-                    semister = val;
-                    print(semister);
-                  });
-                },
-                hint: Text("Semester"),
-                value: semister,
-              ),
-            ],
-              ),
-              SizedBox(
-            height: size.height * 0.01,
-              ),
-              TextFormField(
-            validator: (val) {
-              return isEmail(val)
-                  ? null
-                  : "Enter valid Email Address";
-            },
-            decoration: InputDecoration(
-              hintText: "Email",
-            ),
-            keyboardType: TextInputType.emailAddress,
-            onChanged: (val) {
-              email = val;
-            },
-              ),
-              SizedBox(
-            height: size.height * 0.01,
-              ),
-              TextFormField(
-            obscureText: true,
-            validator: (val) {
-              return val.length < 6 ? "Enter Correct Password" : null;
-            },
-            decoration: InputDecoration(
-              hintText: "Password",
-            ),
-            onChanged: (val) {
-              password = val;
-            },
-              ),
-              SizedBox(
-            height: size.height * 0.035,
-              ),
-              GestureDetector(
-            onTap: () {
-              signUp();
-              print("Clicked on Sign UP Gradient Button");
-            },
-            child: blueButton(context, "Sign Up"),
-              ),
-              SizedBox(
-            height: size.height * 0.02,
-              ),
-              Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              GestureDetector(
-                  onTap: () {
-                    Navigator.pushReplacement(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => SignIn()));
-                    print("Clicked on Sign IN Already have account");
+                width: 152,
+                child: TextFormField(
+                  validator: (val) {
+                    return val.length <= 5 && val.isNotEmpty
+              ? null
+              : "Roll number should be of 2 digits";
                   },
-                  child: Text(
-                    "Already have an Account?",
-                    style: TextStyle(fontSize: 15),
-                  )),
-              GestureDetector(
-                  onTap: () {
-                    Navigator.pushReplacement(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => Home()));
-                    print("Clicked on Sign IN");
-                  },
-                  child: Text(
-                    " Sign In",
-                    style: TextStyle(
-              fontSize: 15,
-              decoration: TextDecoration.underline),
-                  ))
-            ],
-              ),
-              Spacer()
-            ],
-            ),
-                    ),
+                  decoration: InputDecoration(
+                    hintText: "Roll No.(5 digit)",
                   ),
+                  keyboardType: TextInputType.number,
+                  onChanged: (val) {
+                    rollno = val;
+                  },
+                  cursorWidth: 10,
+                ),
+              ),
+              SizedBox(
+                width: 20,
+              ),
+              SizedBox(
+                width: 152,
+                child: TextFormField(
+                  validator: (val) {
+                    return val == "A" || val == 'B'
+              ? null
+              : "Enter either A or B";
+                  },
+                  decoration: InputDecoration(
+                    hintText: "Div.",
+                  ),
+                  
+                  onChanged: (val) {
+                    div = val;
+                  },
+                  cursorWidth: 10,
+                ),
+              ),
+                ]),
+                SizedBox(
+              height: size.height * 0.01,
+                ),
+                ListTile(
+              title: Text(
+                  "Date of Birth : ${pickeddate.day}/${pickeddate.month}/${pickeddate.year}"),
+              trailing: Icon(Icons.keyboard_arrow_down),
+              onTap: _pickDate,
+                ),
+                Row(
+              children: [
+                DropdownButton<String>(
+                  items: [
+                    DropdownMenuItem(
+              value: "Computer Engineering",
+              child: Text("Computer Engineering")),
+                    DropdownMenuItem(
+              value: "E&TC", child: Text("E&TC")),
+                    DropdownMenuItem(
+              value: "Mechanical Engineering",
+              child: Text("Mechanical Engineering")),
+                    DropdownMenuItem(
+              value: "Civil Engineering",
+              child: Text("Civil Engineering")),
+                    DropdownMenuItem(
+              value: "Chemical Engineering",
+              child: Text("Chemical Engineering")),
+                  ],
+                  onChanged: (val) {
+                    setState(() {});
+                    branch = val;
+                    print(branch);
+                  },
+                  hint: Text("Branch"),
+                  value: branch,
+                ),
+                SizedBox(
+                  width: 40,
+                ),
+                DropdownButton<String>(
+                  items: [
+                    DropdownMenuItem(
+              value: "Sem 1", child: Text("Sem 1")),
+                    DropdownMenuItem(
+              value: "Sem 2", child: Text("Sem 2")),
+                    DropdownMenuItem(
+              value: "Sem 3", child: Text("Sem 3")),
+                    DropdownMenuItem(
+              value: "Sem 4", child: Text("Sem 4")),
+                    DropdownMenuItem(
+              value: "Sem 5", child: Text("Sem 5")),
+                    DropdownMenuItem(
+              value: "Sem 6", child: Text("Sem 6")),
+                    DropdownMenuItem(
+              value: "Sem 7", child: Text("Sem 7")),
+                    DropdownMenuItem(
+              value: "Sem 8", child: Text("Sem 8")),
+                  ],
+                  onChanged: (val) {
+                    setState(() {
+                      semister = val;
+                      print(semister);
+                    });
+                  },
+                  hint: Text("Semester"),
+                  value: semister,
+                ),
+              ],
+                ),
+                SizedBox(
+              height: size.height * 0.01,
+                ),
+                TextFormField(
+              validator: (val) {
+                return isEmail(val)
+                    ? null
+                    : "Enter valid Email Address";
+              },
+              decoration: InputDecoration(
+                hintText: "Email",
+              ),
+              keyboardType: TextInputType.emailAddress,
+              onChanged: (val) {
+                email = val;
+              },
+                ),
+                SizedBox(
+              height: size.height * 0.01,
+                ),
+                TextFormField(
+              obscureText: true,
+              validator: (val) {
+                return val.length < 6 ? "Enter Correct Password" : null;
+              },
+              decoration: InputDecoration(
+                hintText: "Password",
+              ),
+              onChanged: (val) {
+                password = val;
+              },
+                ),
+                SizedBox(
+              height: size.height * 0.035,
+                ),
+                GestureDetector(
+              onTap: () {
+                signUp();
+                print("Clicked on Sign UP Gradient Button");
+              },
+              child: blueButton(context, "Sign Up"),
+                ),
+                SizedBox(
+              height: size.height * 0.02,
+                ),
+                Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                GestureDetector(
+                    onTap: () {
+                      Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => SignIn()));
+                      print("Clicked on Sign IN Already have account");
+                    },
+                    child: Text(
+                      "Already have an Account?",
+                      style: TextStyle(fontSize: 15),
+                    )),
+                GestureDetector(
+                    onTap: () {
+                      Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => Home()));
+                      print("Clicked on Sign IN");
+                    },
+                    child: Text(
+                      " Sign In",
+                      style: TextStyle(
+                fontSize: 15,
+                decoration: TextDecoration.underline),
+                    ))
+              ],
+                ),
+                Spacer()
+              ],
+              ),
+                      ),
+                    ),
+          ),
     );
   }
 
